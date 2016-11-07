8c58bd6c9ce1667dae61957edc2a8772646237a3
Ziga Strgar <ziga_strgar@hotmail.com>
2016-11-07 16:06:17 +0100

    Remove git folder

--- skripta.sh
+++ skripta.sh
@@ -10,6 +10,7 @@ sudo apt-get dist-upgrade;
 sudo sed -i 's/DocumentRoot \/home\/ubuntu\/workspace/\DocumentRoot \/home\/ubuntu\/workspace\/public/' /etc/apache2/sites-enabled/001-cloud9.conf;
 sudo service apache2 restart;
 composer install;
-cp .env.example .env
+cp .env.example .env;
 php artisan key:generate;
+sudo rm -rf .git;
 sudo rm skripta.sh
