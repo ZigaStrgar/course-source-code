fbc74fd4a8b36626f8459cf97be609380c6b6039
Ziga Strgar <ziga_strgar@hotmail.com>
2016-11-07 14:43:14 +0100

    Dodana skripta, ki uredi PHP verzijo in ostale stvari glede laravela

---
+++ skripta.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo add-apt-repository ppa:ondrej/php;
+sudo apt-get update;
+sudo apt-get install libapache2-mod-php5.6;
+sudo a2dismod php5;
+sudo a2enmod php5.6;
+sudo add-apt-repository ppa:ondrej/php5-compat;
+sudo apt-get update;
+sudo apt-get dist-upgrade;
+composer install;
+cp .env.example .env
+php artisan key:generate;
