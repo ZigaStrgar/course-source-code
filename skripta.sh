d335466f61e16b6126d9755e991efbb2aa7e60a4
Ziga Strgar <ziga_strgar@hotmail.com>
2016-11-07 15:52:29 +0100

    Changes the document root

--- skripta.sh
+++ skripta.sh
@@ -7,6 +7,9 @@ sudo a2enmod php5.6;
 sudo add-apt-repository ppa:ondrej/php5-compat;
 sudo apt-get update;
 sudo apt-get dist-upgrade;
+sudo sed -i 's/DocumentRoot \/home\/ubuntu\/workspace/\DocumentRoot \/home\/ubuntu\/workspace\/public/' /etc/apache2/sites-enabled/001-cloud9.conf;
+sudo service apache2 restart;
 composer install;
 cp .env.example .env
 php artisan key:generate;
+sudo rm skripta.sh
