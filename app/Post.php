563b16bf6e41dbb6895ff486136e4869766520fd
Ziga Strgar <ziga_strgar@hotmail.com>
2016-11-29 15:25:57 +0100

    Notes 03

--- app/Post.php
+++ app/Post.php
@@ -7,11 +7,26 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('published_desc', function(Builder $builder) {
+            $builder->orderBy('published', 'DESC');
+        });
+    }
+
     use SoftDeletes;
 
     protected $fillable = [ 'title', 'content', 'description', 'slug', 'category_id' ];
 
     protected $dates = [ 'created_at', 'updated_at', 'deleted_at' ];
+
+    public function scopePublished($query)
+    {
+        return $query->where('published', true);
+    }
 
     public function categories()
     {
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,13 +11,28 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\User::class, function(Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ? : $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Post::class, function(Faker\Generator $faker) {
+    return [
+        'title'       => $faker->sentence,
+        'text'        => $faker->sentences(10, true),
+        'description' => $faker->sentences(2, true),
+        'user_id'     => App\User::find(rand(1, 3))->id,
+    ];
+});
+
+$factory->define(App\Category::class, function(Faker\Generator $faker) {
+    return [
+        'name' => $faker->unique()->word
     ];
 });
